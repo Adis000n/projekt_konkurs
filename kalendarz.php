666df78d3c46e96635fe99125ea669f6e312b985
KacperV07 <99750904+KacperV07@users.noreply.github.com>
2023-10-19 20:30:29 +0200

    Kalendarz + style

---
+++ kalendarz.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
+    <link type="text/css" rel="stylesheet" href="style.css">
+    <title>Kalendarz</title>
+</head>
+<body>
+    <div class="menu">
+        <button class="menu_btn"></button>
+        <button class="newViewBtn">widok1</button>
+        <button class="newViewBtn">widok2</button>
+        <button class="newViewBtn">widok3</button>
+        <button class="menu_btn"></button>
+        
+
+    </div>
+    
+</body>
+</html>
