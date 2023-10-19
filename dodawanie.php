477541f4528b74ac752b272718566d91c1cca241
adrian szczotka <zapasowyadi@gmail.com>
2023-10-19 22:15:26 +0200

    dlaej formularz, kalendarz działający 1 przycisk

--- dodawanie.php
+++ dodawanie.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dodawanie elementów</title>
+    <title>Dodawanie wydarzeń</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
@@ -18,45 +18,80 @@
     <form method="post">
     <!-- potrzebna nazwa, co to jest(kartkówka,sprawdzian,czy zadanie), jak ważne, komentarz, data na kiedy, i od kiedy do kiedy chcesz to robić -->
     <div class="form-floating mb-3">
-        <input class="form-control" id="floatingInput" placeholder="Przykłądowy_przedmiot">
+        <input class="form-control" id="floatingInput" placeholder="Przykładowy_przedmiot" name="nazwa">
         <label for="floatingInput">Nazwa elementu</label>
     </div>
     <div class="form-floating">
-    <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
-        <option selected>Klinij aby otworzyć menu</option>
+    <select class="form-select" id="floatingSelect" aria-label="Floating label select example" name="typ">
+        <option selected disabled>Wybierz typ elementu</option>
         <option value="sprawdzian">Sprawdzian</option>
         <option value="kartkowka">Kartkówka</option>
         <option value="zadanie">Zadanie domowe</option>
+        <option value="przypomnienie">Przypomnienie</option>
+        <option value="obowiazek">Obowiązek domowy</option>
     </select>
-    <label for="floatingSelect">Wybierz typ elementu</label>
+    <label for="floatingSelect">Klinij aby otworzyć menu </label>
     </div>
     <br>
     <div class="form-floating">
-    <select class="form-select" id="floatingSelect2" aria-label="Floating label select example">
-        <option selected>Klinij aby otworzyć menu</option>
+    <select class="form-select" id="floatingSelect2" aria-label="Floating label select example" name="waznosc">
+        <option selected disabled>Wybierz jak ważny jest element</option>
         <option value="3">Bardzo ważny</option>
         <option value="2">Średnio ważny</option>
         <option value="1">Mało ważny</option>
     </select>
-    <label for="floatingSelect2">Wybierz jak ważny jest element</label>
+    <label for="floatingSelect2">Klinij aby otworzyć menu</label>
     </div>
     <br>
     <div class="mb-0">
     <label for="floatingSelect">Data elementu</label>
-    <input class="form-control form-control-solid" placeholder="Pick date rage" id="kt_daterangepicker_3"/>
-    </div>
-    <div class="mb-0">
-    <label for="floatingSelect">Data elementu</label>
-    <input class="form-control form-control-solid" placeholder="Pick date rage" id="kt_daterangepicker_1"/>
+    <input class="form-control form-control-solid" placeholder="Pick date rage" id="kt_daterangepicker_3" name="data"/>
     </div>
     <br>
     <div class="form-floating">
-        <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
+    <select class="form-select" id="floatingSelect" disabled aria-label="Floating label select example">
+        <option selected disabled>Ile dni przed chcesz się uczyć/robić</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+    </select>
+    <label for="floatingSelect">Kliknij aby otworzyć menu</label>
+    </div>
+    <br>
+    <div class="form-check">
+    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+    <label class="form-check-label" for="exampleRadios1">
+        tylko jeden dzień tyle przed ile wybrałeś
+    </label>
+    </div>
+    <div class="form-check">
+    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+    <label class="form-check-label" for="exampleRadios2">
+        Przez wszystkie dni przed tyle ile wybrałeś
+    </label>
+    </div>
+    <br>
+    <div class="form-floating">
+        <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px" name="komentarz"></textarea>
         <label for="floatingTextarea2">Komentarz</label>
     </div>
     <br>
     <input class="btn btn-primary" type="submit" value="Submit" style="width:100%">
     </form>
+    <?php
+    if(isset($_POST["nazwa"])){
+        $nazwa = $_POST["nazwa"];
+        $typ = $_POST["typ"];
+        $waznosc = $_POST["waznosc"];
+        $data = $_POST["data"];
+        $komentarz = $_POST["komentarz"];
+        echo $nazwa." ".$typ." ".$waznosc." ".$data." ".$data_od_do." ".$komentarz;   
+    }
+    ?>
 </body>
 <style>
     #back{
@@ -84,7 +119,7 @@
 </style>
 <script>
     function goBack(){
-        location.href = "index.php";
+        location.href = "kalendarz.php";
     }
     $("#kt_daterangepicker_3").daterangepicker({
         singleDatePicker: true,
@@ -92,6 +127,6 @@
         minYear: 1901,
         maxYear: parseInt(moment().format("YYYY"),12)
     });
-    $("#kt_daterangepicker_1").daterangepicker();
+
 </script>
 </html>
--- kalendarz.php
+++ kalendarz.php
@@ -4,11 +4,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">    
     <link type="text/css" rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <title>Kalendarz</title>
 </head>
 <body>
     <div class="menu">
-        <button class="menu_btn"></button>
+        <button class="menu_btn" onclick="goToDodawanie()"></button>
         <button class="newViewBtn">widok1</button>
         <button class="newViewBtn">widok2</button>
         <button class="newViewBtn">widok3</button>
@@ -18,4 +20,9 @@
     </div>
     
 </body>
+<script>
+        function goToDodawanie(){
+        location.href = "dodawanie.php";
+    }
+</script>
 </html>
