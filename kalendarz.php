e47bb2b8cd759b9bda095bc790722c3a38fd5cd8
Kuba <kubustomi2007@gmail.com>
2023-10-22 12:09:16 +0200

    Przyciski i ustawienia uzytkownika

--- kalendarz.php
+++ kalendarz.php
@@ -6,6 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link type="text/css" rel="stylesheet" href="style.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <title>Kalendarz</title>
 </head>
 <body>
@@ -13,10 +14,18 @@
    <button onclick="topFunction()" id="goUpBtn" title="Go to top">Do Góry!</button>
     <div class="menu">
         <button class="menu_btn" onclick="goToDodawanie()"></button>
-        <button class="newViewBtn">widok1</button>
-        <button class="newViewBtn">widok2</button>
-        <button class="newViewBtn">widok3</button>
-        <button class="menu_btn"></button>       
+        <button  class="btn btn-secondary dropdown-toggle"   data-bs-theme="dark" type="button"  style="width:30vh;height:45px">widok1</button>
+        <button  class="btn btn-secondary dropdown-toggle"   data-bs-theme="dark" type="button"  style="width:30vh;height:45px">widok2</button>
+        <button  class="btn btn-secondary dropdown-toggle"   data-bs-theme="dark" type="button"  style="width:30vh;height:45px">widok3</button>
+        <div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle"   data-bs-theme="dark" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="width:35vh">
+     <img src="img/awatar.png" width="10% "  height="10%"> Nazwa uzytkownika
+      </button>
+  <ul class="dropdown-menu">
+    <li><button class="dropdown-item" type="button" onclick="document.location=">Ustawienia użytkownika</button></li>
+<li><button  class="dropdown-item" onclick="alert()">Wyloguj się</button></li>
+</ul>
+</div>
 
     </div>
     <div class="calendar">
@@ -69,6 +78,33 @@
     
 </body>
 <script>
+
+  //funkcja od wylogowywania sie
+
+function alert(){
+    Swal.fire({
+  title: 'Jestes pewien?',
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Tak',
+  cancelButtonText: 'Nie'
+}).then((result) => {
+  if (result.isConfirmed) {
+    Swal.fire(
+      'Wylogowano!',
+    )
+  }
+})
+    }
+documnt.getElementById('normalny').addEventListener('click',function(){
+    alert('Alert zwykly')
+})
+
+
+
+
         function goToDodawanie(){
         location.href = "dodawanie.php";
     }
