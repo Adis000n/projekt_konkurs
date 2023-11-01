546dbf3f3cdea98546d7622aa7a126695bb71426
Kuba <kubustomi2007@gmail.com>
2023-11-01 15:33:16 +0100

    Dziala zmiana hasla

--- haslo.php
+++ haslo.php
@@ -3,26 +3,33 @@ session_start();
 $con = mysqli_connect("localhost","root","","konkurs");
 if(isset($_POST['zapisz1'])) {
     $id = $_SESSION['id'];
-    $dobrehaslo = $_SESSION['password'];
     $obecne_haslo = $_POST['obecne_haslo'];
     $nowe_haslo = $_POST['nowe_haslo'];
     $nowe_haslo2 = $_POST['nowe_haslo2'];
-
-    if (!empty($obecne_haslo) && !empty($nowe_haslo) && !empty($nowe_haslo2)) {
-        if ($obecne_haslo == $dobrehaslo) {
+    $sql = "SELECT `password` from  user where id = '$id' ";
+    $result = mysqli_query($con,$sql);
+    if ($result && mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $stored_password = $row['password'];
+        
+        if (password_verify($obecne_haslo, $stored_password)) {
             if ($nowe_haslo == $nowe_haslo2) {
-                $query = "UPDATE user SET password='$nowe_haslo2' WHERE id='$id' ";
+                $password_hash = password_hash($nowe_haslo, PASSWORD_DEFAULT);
+                $query = "UPDATE user SET password='$password_hash' WHERE id='$id' ";
             } else {
                 $_SESSION['status'] = "Hasło nowe nie zgadza się";
                 header("Location:myaccount.php");
+                exit();
             }
         } else {
             $_SESSION['status'] = "Obecne hasło nie zgadza się";
             header("Location:myaccount.php");
+            exit();
         }
     } else {
         $_SESSION['status'] = "Niepowodzenie z niewiadomych przyczyn";
         header("Location:myaccount.php");
+        exit();
     }
 
     if(isset($query) && !empty($query)) {
@@ -30,9 +37,15 @@ if(isset($_POST['zapisz1'])) {
         if($query_run) {
             $_SESSION['status'] = "Pomyślnie zaaktualizowano hasło!";
             header("Location:myaccount.php");
-        } 
+            exit();
+        } else {
+            $_SESSION['status'] = "Błąd podczas aktualizowania hasła: " . mysqli_error($con);
+            header("Location:myaccount.php");
+            exit();
+        }
     }
 }
 ?>
+
 
 
