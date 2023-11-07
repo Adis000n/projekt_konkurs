14dd976ee3980035076c4863c6ed82d30867db13
adrian szczotka <zapasowyadi@gmail.com>
2023-11-07 20:39:42 +0100

    poprawa

--- db.php
+++ db.php
@@ -1,8 +1,4 @@
 <?php
-$con = mysqli_connect("46.242.239.199:3380","37911724_konkurs","kH71xFVYZ");
-mysqli_select_db($con,"37911724_konkurs");
-
-if (!$con) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+$con = mysqli_connect("localhost","root","");
+mysqli_select_db($con,"konkurs");
 ?>
